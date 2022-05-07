94f28aa1035321f628e6dc87ffad81b2b2d41d8a
shaikngit <shaikn@microsoft.com>
2022-05-07 05:43:58 +0530

    fixing providers.tf files

--- AzureLoadbalancer_WindowsBE/main.tf
+++ AzureLoadbalancer_WindowsBE/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-    subscription_id = var.subId
-    client_id = var.client_Id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-}
-
-
 resource "azurerm_public_ip" "main" {
   name                = "PublicIPForLB"
   location            = var.location
